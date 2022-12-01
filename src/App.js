ddc0d72a7583bda4d5f7b34cca324ce40d120f9a
tedspare <ted.spare@gmail.com>
2022-12-01 10:50:00 -0500

    Add gif grid and input

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import React, { useEffect, useState } from "react";
 
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+  "https://media.giphy.com/media/Ru9sjtZ09XOEg/giphy.gif",
+  "https://media.giphy.com/media/3o8doR2qGIXQDGCVoY/giphy.gif",
+  "https://media.giphy.com/media/l0Iy3yLzaEPZK4OCA/giphy.gif",
+  "https://media.giphy.com/media/eR8bS1SsJGloeK8T4z/giphy.gif",
+];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
 
   const checkIfWalletIsConnected = async () => {
     if (window?.solana?.isPhantom) {
       const response = await window.solana.connect({ onlyIfTrusted: true });
-      console.log("Connected with Public Key:", response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
     } else {
       alert("Solana object not found! Get a Phantom Wallet 👻");
@@ -21,8 +28,16 @@ const App = () => {
   const connectWallet = async () => {
     if (window?.solana) {
       const response = await window.solana.connect();
-      console.log("Connected with Public Key:", response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      setGifList([...gifList, inputValue]);
+      setInputValue("");
+    } else {
+      console.log("Empty input. Try again.");
     }
   };
 
@@ -33,6 +48,17 @@ const App = () => {
     window.addEventListener("load", onLoad);
     return () => window.removeEventListener("load", onLoad);
   }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list...");
+
+      // Call Solana program here.
+
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
 
   return (
     <div className="App">
@@ -45,9 +71,10 @@ const App = () => {
             display: "flex",
             alignItems: "center",
             borderRadius: "4rem",
+            position: "absolute",
             padding: "1rem",
-            margin: "1rem",
-            marginLeft: "auto",
+            top: "1rem",
+            right: "1rem",
           }}
         >
           <div
@@ -58,13 +85,13 @@ const App = () => {
               background: "green",
               marginRight: "0.5rem",
             }}
-          ></div>
+          />
           {walletAddress.substr(0, 5)}...{walletAddress.substr(-5)}
         </div>
       )}
       <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🖼 Giftr</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
@@ -77,6 +104,33 @@ const App = () => {
             </button>
           )}
         </div>
+        {walletAddress && (
+          <div className="connected-container">
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                sendGif();
+              }}
+            >
+              <input
+                onChange={(e) => setInputValue(e.target.value)}
+                value={inputValue}
+                type="text"
+                placeholder="Enter gif link!"
+              />
+              <button type="submit" className="cta-button submit-gif-button">
+                Submit
+              </button>
+            </form>
+            <div className="gif-grid">
+              {gifList.reverse().map((gif) => (
+                <div className="gif-item" key={gif}>
+                  <img src={gif} alt={gif} />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
